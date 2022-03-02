4e6fea599fcc677bda3f8b667671c2ac3e0471e9
Xavier Fernandez <lain@MacBook-Pro-3.home>
2022-03-02 02:01:17 -0500

    fleshing out classes

---
+++ backend/src/main/java/onramp/assessment/backend/model/AudioUpload.java
@@ -0,0 +1,61 @@
+package onramp.assessment.backend.model;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+import java.util.ArrayList;
+
+@Document(collection = "tracks")
+public class AudioUpload {
+
+    @Id
+    private String id; 
+    private String name; 
+    private String description; 
+    private String image; 
+    private int upvotes; 
+    private int downvotes; 
+    private String source; 
+    private String audio; 
+
+    public AudioUpload(String name, String description, String image, String source, String audio ) {
+        this.name = name; 
+        this.description = description; 
+        this.image = image; 
+        this.upvotes = 0; 
+        this.downvotes = 0;
+        this.source = source; 
+        this.audio = audio;
+    } 
+
+    public String getId() {
+        return id;
+    } 
+
+    public String getName() {
+        return name;
+    } 
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImage() {
+        return image;
+    } 
+
+    public ArrayList<Integer> getVotes(){
+        ArrayList<Integer> votes = new ArrayList<Integer>(); 
+        votes.add(upvotes); 
+        votes.add(downvotes);
+        return votes;
+    }
+
+    public String getSource() {
+        return source;
+    } 
+
+    public String getAudio() {
+        return audio;
+    }
+}
