28b0f670b1add50762395400ea56a15ddb4038fb
Xavier Fernandez <lain@MacBook-Pro-3.home>
2022-03-02 04:45:09 -0500

    some more backend stuff

--- backend/src/main/java/onramp/assessment/backend/controller/AudioController.java
+++ backend/src/main/java/onramp/assessment/backend/controller/AudioController.java
@@ -1,5 +1,6 @@
 package onramp.assessment.backend.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import onramp.assessment.backend.repository.AudioRepository;
@@ -11,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,8 +25,19 @@ public class AudioController {
     private AudioRepository repo; 
 
     @GetMapping("/listall")
-    public List<AudioUpload> listAll(){
-        return repo.findAll();
+    public ResponseEntity<List<AudioUpload>> listAll(){
+        try { 
+            List<AudioUpload> audios = new ArrayList<AudioUpload>();
+            repo.findAll().forEach(audios::add); 
+            if (audios.isEmpty()) {
+                return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(audios, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+
     }
 
     @PostMapping("/submit")
@@ -36,6 +48,16 @@ public class AudioController {
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @PatchMapping("/upvote/{id}") 
+    public ResponseEntity<HttpStatus> upVote(@PathVariable("id") String id){
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PatchMapping("/downvote/{id}") 
+    public ResponseEntity<HttpStatus> downVote(@PathVariable("id") String id){
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
