0160e17a9158949c03bcd4f37f838c81e15e1e90
Xavier Fernandez <lain@MacBook-Pro-3.home>
2022-03-07 08:32:51 -0500

    more frontend scaffolding, mostly setting up controller part of mvc

---
+++ frontend/bulletin-frontend/utils/model_helpers/audio_response.ts
@@ -0,0 +1,16 @@
+
+type AudioResponse = {
+    id: string, 
+    name: string, 
+    description: string, 
+    title: string, 
+    image: string, 
+    upvotes: number, 
+    downvotes: number, 
+    source: string, 
+    audio: string, 
+    translation: string[], 
+    validUpload: boolean,
+}
+
+export default AudioResponse;
---
+++ frontend/bulletin-frontend/utils/model_helpers/audio_upload_class.ts
@@ -0,0 +1,79 @@
+import * as z from 'zod'
+
+class AudioUpload{
+    _name: string; 
+    _description: string; 
+    _image: string; 
+    _source: string; 
+    _audio: string;
+    _title: string; 
+
+    constructor(
+            name: string,
+            description: string, 
+            image: string, 
+            source: string, 
+            audio: string, 
+            title: string
+        )
+    {
+        this._name = name; 
+        this._description = description; 
+        this._image = image;
+        this._source = source; 
+        this._audio = audio; 
+        this._title = title;
+    } 
+
+    getName(): string {
+        return this._name;
+    } 
+
+    getDescription(): string {
+        return this._description;
+    } 
+
+    getImage(): string {
+        return this._image;
+    } 
+
+    getSource(): string {
+        return this._source;
+    } 
+
+    getAudio(): string {
+        return this._audio;
+    }
+
+    getTitle(): string {
+        return this._title;
+    }
+
+    validate(): boolean {
+        const uploadBody = z.object({
+            name: z.string().min(1), 
+            description: z.string().min(1), 
+            image: z.string().url().min(1), 
+            source: z.string().url().min(1),  
+            audio: z.string().url().min(1), 
+            title: z.string().min(1)
+        }); 
+
+        const valid = uploadBody.safeParse({
+            name: this._name, 
+            description: this._description, 
+            image: this._image,
+            source: this._source, 
+            audio: this._audio,
+            title: this._title
+        }); 
+
+        if (!valid.success){ return false;};
+        return true;
+    }
+
+}
+
+export default AudioUpload;
+
+
